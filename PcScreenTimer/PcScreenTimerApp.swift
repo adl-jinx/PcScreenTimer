0b6d834c80c8f8c86b75a355fd5836c638e03816
adl-jinx <114005033+adl-jinx@users.noreply.github.com>
2023-03-04 17:01:30 +0100

    Initial Commit

---
+++ PcScreenTimer/PcScreenTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PcScreenTimerApp.swift
+//  PcScreenTimer
+//
+//  Created by Antonio De Luca on 04/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PcScreenTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
